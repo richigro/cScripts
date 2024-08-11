8afecd74492820662478613a36575889a018618d
Ricardo Guerrero <richi_gro@hotmail.com>
2024-08-11 14:51:59 -0500

    do it in a while loop

--- countCards.c
+++ countCards.c
@@ -26,29 +26,33 @@ int main()
   puts("Enter the card_name: ");
   scanf("%2s", card_name);
   char card_value = card_name[0];
-  int val = 0;
   char value_ten_cards[] = {'K', 'Q', 'J'};
+  int val = 0;
 
-  if (value_in_array(card_value, value_ten_cards, sizeof(value_ten_cards)))
+  while (card_value != 'X')
   {
-    val = 10;
+    if (value_in_array(card_value, value_ten_cards, sizeof(value_ten_cards)))
+    {
+      val = 10;
+    }
+    else if (card_value == 'A')
+    {
+      val = 11;
+    }
+    else
+    {
+      val = atoi(card_name);
+    }
+    // Check if the value is between 3 to 6
+    if (val >= 3 && val <= 6)
+    {
+      puts("count has gone up");
+    }
+    else if (val == 10)
+    {
+      puts("count has gone down");
+    }
   }
-  else if (card_value == 'A')
-  {
-    val = 11;
-  }
-  else
-  {
-    val = atoi(card_name);
-  }
-  // Check if the value is between 3 to 6
-  if (val >= 3 && val <= 6)
-  {
-    puts("count has gone up");
-  }
-  else if (val == 10)
-  {
-    puts("count has gone down");
-  }
+
   return 0;
 }
