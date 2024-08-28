e6fec7d2d4f8bf9c5735513838bf5ffb5392ff91
Ricardo Guerrero <richi_gro@hotmail.com>
2024-08-27 23:27:22 -0500

    support argv

--- fileStreams.c
+++ fileStreams.c
@@ -1,24 +1,28 @@
 /*
   This i sht code to real all of the dat from a GPS file
   and write data into one of 3 files
+
+  Example usage:
+  $ ./main mermaid marmaid.csv elvis elvises.csv the_rest.csv
+
 */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-int main()
+int main(int argc, char *argv[])
 {
   char line[80];
   FILE *in = fopen("spooky.csv", "r");
-  FILE *file1 = fopen("ufos.csv", "w");
-  FILE *file2 = fopen("disappearances.csv", "w");
-  FILE *file3 = fopen("others.csv", "w");
+  FILE *file1 = fopen(argv[2], "w");
+  FILE *file2 = fopen(argv[4], "w");
+  FILE *file3 = fopen(argv[5], "w");
 
   while (fscanf(in, "%79[^\n]\n", line) == 1)
   {
-    if (strstr(line, "UFO"))
+    if (strstr(line, argv[1]))
       fprintf(file1, "%s\n", line);
-    else if (strstr(line, "Disappearance"))
+    else if (strstr(line, argv[3]))
       fprintf(file2, "%s\n", line);
     else
       fprintf(file3, "%s\n", line);
