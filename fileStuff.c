191a1f871a70fdcd7b6f46d909a3918e2852e219
Ricardo Guerrero <richi_gro@hotmail.com>
2024-08-27 22:13:56 -0500

    reading stuff

--- fileStuff.c
+++ fileStuff.c
@@ -5,12 +5,21 @@
 
 int main()
 {
-  char text[80];
+  char buffer[256];
   // Read from file
   FILE *in_file = fopen("test.txt", "r");
 
   // Display the text inside of this file.
-  fprintf(in_file, "%79[^\n]", text);
+  fprintf(in_file, "%79[^\n]", buffer);
+
+  if (in_file == NULL)
+  {
+    fprintf(stderr, "Error: could not open file.\n");
+    return 1;
+  }
+
+  // fgets(buffer, sizeof(buffer), in_file);
+  printf("the line of text is: %s", buffer);
 
   return 0;
 }
