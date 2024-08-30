7e8077da6cb4bdf5e5668a6fe68eb4fcfeb58868
Ricardo Guerrero <richi_gro@hotmail.com>
2024-08-29 20:40:35 -0500

    try this

--- pizza.c
+++ pizza.c
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
   int count = 0;
   char ch;
 
-  while ((ch = getopt(argc, argv, "d:t")) != EOF)
+  while ((ch = getopt(argc, argv, "d:t")) != -1)
   {
 
     switch (ch)
