07e3dc5ba4a6c84d533d7c1f0c71b85ca3990921
Ricardo Guerrero <richi_gro@hotmail.com>
2024-09-01 11:29:50 -0500

    size of other dat types

--- sizeOfDataTypes.c
+++ sizeOfDataTypes.c
@@ -8,7 +8,12 @@
 int main()
 {
 
-  printf("the size of an int is: %zu\n", sizeof(int));
+  printf("the size of an int is: %lu bytes or %lu bits\n", sizeof(int), sizeof(int) * 8);
+  printf("the size of an short is: %lu bytes or %lu bits\n", sizeof(short), sizeof(short) * 8);
+  printf("the size of an double is: %lu bytes or %lu bits\n", sizeof(double), sizeof(double) * 8);
+  printf("the size of an long is: %lu bytes or %lu bits\n", sizeof(long), sizeof(long) * 8);
+  printf("the size of an float is: %lu bytes or %lu bits\n", sizeof(float), sizeof(float) * 8);
+  printf("the size of an char is: %lu bytes or %lu bits\n", sizeof(char), sizeof(char) * 8);
 
   return 0;
 }
