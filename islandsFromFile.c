245b7f89bfaaafe9360acaeea083c8fa4be10d8c
Ricardo Guerrero <richi_gro@hotmail.com>
2024-09-15 14:06:03 -0500

    test

--- islandsFromFile.c
+++ islandsFromFile.c
@@ -1,13 +1,33 @@
 #include <stdio.h>
 
+typedef struct island
+{
+  char *name;
+  char *opens;
+  char *closes;
+  struct island *next;
+} island;
+
+void display(island *first)
+{
+  // loop through each isand and print it
+  while (first != NULL)
+  {
+    printf("Hello: %s\n", first->name);
+  }
+}
+
 int main()
 {
   char name[80];
+  island *start = NULL;
 
   while (fgets(name, sizeof(name), stdin) != NULL)
   {
     // hello
     printf("Hello the current line: %s\n", name);
   }
+
+  display(start);
   return 0;
 }
